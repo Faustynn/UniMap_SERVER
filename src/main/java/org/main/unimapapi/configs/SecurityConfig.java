96c656db2dce2fc959e99ea6e031e16dec45571a
Nazar Faustyn <153837991+Faustynn@users.noreply.github.com>
2025-03-05 01:36:46 +0100

    Start Comment logic

--- src/main/java/org/main/unimapapi/configs/SecurityConfig.java
+++ src/main/java/org/main/unimapapi/configs/SecurityConfig.java
@@ -40,8 +40,10 @@ public class SecurityConfig {
                                         "/api/unimap_pc/user/create",
                                         "/api/unimap_pc/refresh",
                                         "/api/unimap_pc/resources/**",
-                                         "/api/unimap_pc/news/all"
-                                         // TODO: error page "/error"
+                                        "/api/unimap_pc/comments/teacher/**",
+                                        "/api/unimap_pc/comments/subject/**",
+                                        "/api/unimap_pc/news/all"
+                                         // TOD O: error page "/error"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
