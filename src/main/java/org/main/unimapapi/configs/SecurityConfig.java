69ecbf5ee49e63303d09acbb25660b6eb3e97128
Viktor Hutsuliak <113915580+Vtoshik@users.noreply.github.com>
2025-03-06 13:27:35 +0100

    Logs added

--- src/main/java/org/main/unimapapi/configs/SecurityConfig.java
+++ src/main/java/org/main/unimapapi/configs/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                                         "/api/unimap_pc/user/create",
                                         "/api/unimap_pc/refresh",
                                         "/api/unimap_pc/resources/**",
-                                         "/api/unimap_pc/news/all"
+                                         "/api/unimap_pc/news/all",
+                                        "/api/unimap_pc/log"
                                          // TODO: error page "/error"
                                 ).permitAll()
                                 .anyRequest().authenticated()
